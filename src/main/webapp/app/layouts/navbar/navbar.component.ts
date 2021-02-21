387acc41ffe02338a5da74b16e557a9f37c6a9c9
drasko-kosovic <drasko.kosovic@gmail.com>
2021-02-21 22:08:17 +0100

    detail

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -21,6 +21,7 @@ export class NavbarComponent implements OnInit {
   languages = LANGUAGES;
   swaggerEnabled?: boolean;
   version: string;
+  isAuthentical: boolean | undefined;
 
   constructor(
     private loginService: LoginService,
